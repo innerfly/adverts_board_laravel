2e74bfa89f033698194b34d29e2e9add5c9cdba7
innerfly <v.sukhoff@gmail.com>
2019-06-05 20:51:10 +0300

    installed breadcrumbs
    installed breadcrumbs

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,32 @@
+<?php
+
+use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator as Crumbs;
+
+Breadcrumbs::for('home', function (Crumbs $crumbs) {
+    $crumbs->push('Home', route('home'));
+});
+
+Breadcrumbs::for('login', function (Crumbs $crumbs) {
+    $crumbs->parent('home');
+    $crumbs->push('Login', route('login'));
+});
+
+Breadcrumbs::for('register', function (Crumbs $crumbs) {
+    $crumbs->parent('home');
+    $crumbs->push('Register', route('register'));
+});
+
+Breadcrumbs::for('password.request', function (Crumbs $crumbs) {
+    $crumbs->parent('login');
+    $crumbs->push('Reset Password', route('password.request'));
+});
+
+Breadcrumbs::for('password.reset', function (Crumbs $crumbs) {
+    $crumbs->parent('password.request');
+    $crumbs->push('Change', route('password.reset'));
+});
+
+Breadcrumbs::for('cabinet', function (Crumbs $crumbs) {
+    $crumbs->parent('home');
+    $crumbs->push('Cabinet', route('cabinet'));
+});
