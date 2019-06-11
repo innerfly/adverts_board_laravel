a041180414177c92fd053fbbde71399d2114cb7e
innerfly <v.sukhoff@gmail.com>
2019-06-11 13:41:42 +0300

    added profile pages

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -30,10 +30,24 @@ Breadcrumbs::for('password.reset', function (Crumbs $crumbs) {
     $crumbs->push('Change', route('password.reset'));
 });
 
-Breadcrumbs::for('cabinet', function (Crumbs $crumbs) {
+// Cabinet
+
+Breadcrumbs::register('cabinet.home', function (Crumbs $crumbs) {
     $crumbs->parent('home');
-    $crumbs->push('Cabinet', route('cabinet'));
+    $crumbs->push('Cabinet', route('cabinet.home'));
 });
+
+Breadcrumbs::register('cabinet.profile.home', function (Crumbs $crumbs) {
+    $crumbs->parent('cabinet.home');
+    $crumbs->push('Profile', route('cabinet.profile.home'));
+});
+
+Breadcrumbs::register('cabinet.profile.edit', function (Crumbs $crumbs) {
+    $crumbs->parent('cabinet.profile.home');
+    $crumbs->push('Edit', route('cabinet.profile.edit'));
+});
+
+// Admin
 
 Breadcrumbs::for('admin.home', function (Crumbs $crumbs) {
     $crumbs->parent('home');
