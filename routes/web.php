b9ab7e937a6d7df34a2879341c53b3b3311941e5
innerfly <v.sukhoff@gmail.com>
2019-06-12 14:53:50 +0300

    added filled profile middleware added cabinet adverts page

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,8 @@ Route::group(
 
             Route::post('/phone/auth', 'PhoneController@auth')->name('phone.auth');
         });
+
+        Route::resource('adverts', 'Adverts\AdvertController');
     }
 );
 
